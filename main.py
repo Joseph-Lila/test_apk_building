22c5fd0ebda6f772ee37d26d93d1c119b0129c5e
Artur Prakapenka <arturprokopenko01@gmail.com>
2023-01-24 12:01:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+import datetime
+
+from kivy.uix.button import Button
+from kivymd.app import MDApp
+
+
+class KivyApp(MDApp):
+    title = 'Task Distributor'
+
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.primary_hue = "300"
+        self.theme_cls.material_style = "M3"
+        return Button(text=str(datetime.datetime.now()))
+
+
+if __name__ == '__main__':
+    asyncio.run(KivyApp().async_run(async_lib='asyncio'))
